cae140d633e83f01285f294d19cc609ece70827d
Sravik-Sajja <sravik.sajja@gmail.com>
2026-04-18 16:57:48 -0700

    Initial commit

---
+++ server/api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class RequestData(BaseModel):
+    message: str
+
+@app.post("/initial")
+def health_check(data: RequestData):
+    return {"received": data.message}
